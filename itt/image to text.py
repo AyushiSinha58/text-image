a09f19df1c196ea1d16e0acd07ec9d46a409d3cd
AyushiSinha58 <ayushisinha53058@gmail.com>
2024-12-23 19:30:26 +0530

    py

--- itt/image to text.py
+++ itt/image to text.py
@@ -2,7 +2,7 @@ import torch
 from PIL import Image
 import pytesseract
 
-
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 image_path = r"C:\Users\Ayushi\Desktop\GitStuff\TEXT TO IMAGE\text-image\itt\practice image.jpg"
 
 img = Image.open(image_path)
